6134c6ea9de72c4c7fd666b124ef9ae86de1e565
karikrabbi <kari.hrafnkellgunnarsson@elev.ga.ntig.se>
2026-05-21 08:48:53 +0200

    Update java.js

--- java.js
+++ java.js
@@ -1,7 +1,7 @@
-const button = document.getElementById("dropdownbutton");
-const dropdown = document.getElementById("dropdown");
+const button = document.getElementById("dropdownbutton"); //döp html knapp till button
+const dropdown = document.getElementById("dropdown"); //döp html dropdownknapp till dropdown
 
-button.addEventListener("click", function() {
+button.addEventListener("click", function() { //Lyssna efter ett klick på button elementen
   if (dropdown.style.display === "block") {
     dropdown.style.display = "none";
   } else {
@@ -10,22 +10,22 @@ button.addEventListener("click", function() {
 });
 
 
-let pris=0
+let pris=0 //skapa pris variabel
 
-document.getElementById("pris").innerHTML = pris;
-document.getElementById("button1a").addEventListener("click", function(){
-  pris=pris+500;
-  document.getElementById("pris").innerHTML = pris;
+document.getElementById("pris").innerHTML = pris; //hämta pris värde
+document.getElementById("button1a").addEventListener("click", function(){ //lyssna efter klick på add knappen på första varan
+  pris=pris+500; //lägg till 500kr till pris variabeln
+  document.getElementById("pris").innerHTML = pris; //uppdatera
 })
-document.getElementById("button1r").addEventListener("click", function(){
-  pris=pris-500;
+document.getElementById("button1r").addEventListener("click", function(){ //lyssna efter klick på remove knappen på första varan
+  pris=pris-500; //ta bort 500kr från pris variabeln
   document.getElementById("pris").innerHTML = pris;
-  if (pris < 0) {
+  if (pris < 0) { //om pris blir negativt, så ställs priset till 0
     pris = 0;
-    document.getElementById("pris").innerHTML = pris;
+    document.getElementById("pris").innerHTML = pris; //uppdatera
   }
 })
-
+          //samma för alla andra varor
 
 document.getElementById("pris").innerHTML = pris;
 document.getElementById("button2a").addEventListener("click", function(){
